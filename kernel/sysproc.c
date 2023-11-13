00fbf33272b5823d9eea2619c753b87007933449
xh <freescience.iq@gmail>
2023-11-13 23:23:05 +0800

    traps done

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,9 +47,8 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  myproc()->sz = addr + n; // added
-  // if(growproc(n) < 0)
-  //   return -1;
+  if(growproc(n) < 0)
+    return -1;
   return addr;
 }
 
